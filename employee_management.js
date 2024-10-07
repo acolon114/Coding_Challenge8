1dcd5de39aa237f1226f392c64db6aabef8579c5
acolon114 <acolon114@usf.edu>
2024-10-07 01:41:02 +0000

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -12,3 +12,22 @@ class Employee {
         console.log(`Employee ${this.name} makes $${this.salary} employed as a(n) ${this.department} ${this.position}.`);
     }
 };
+
+// Task 2: Create departments
+class Department {
+    constructor(name, employees) {
+        this.name = name;
+        this.employees = [];
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+
+    getDepartmentSalary() {
+        let totalSalary = 0
+        for (const employee of this.employees) {
+            totalSalary += employee.salary
+        }
+        return totalSalary;
+    }
